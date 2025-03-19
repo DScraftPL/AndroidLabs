14332fc70bbccf6d30d26ffcf4182dd1caa5897a
Kacper Wiącek <xxxwiacekxxx@gmail.com>
2025-03-19 14:40:16 +0100

    Lab2 in progress

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lab1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_sub1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.lab1){
+            Intent intencja = new Intent(MainActivity.this, Lab2Activity.class);
+            startActivity(intencja);
+            return true;
+        }
+        if(item.getItemId() == R.id.lab2){
+            Intent intencja = new Intent(MainActivity.this, Lab1Activity.class);
+            startActivity(intencja);
+            return true;
+        }
+        return false;
+    }
+}
